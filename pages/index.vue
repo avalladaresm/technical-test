970db4a479372e2fa04bc6295cef919c778daf7c
Alejandro Valladares <alejo.valladares14@gmail.com>
2023-07-04 18:22:50 -0600

    Added loading state to buttons

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,46 @@
 <script setup>
   const { data: tasks, refresh } = await useFetch('/api/task', { key: "tasks" })
   const newTask = ref('')
+  const isCompleting = ref(false)
+  const isCompletingUuid = ref("")
+  const isDeleting = ref(false)
+  const isDeletingUuid = ref("")
+  const isAdding = ref(false)
 
   async function addTask() {
+    isAdding.value = true
     await useFetch('/api/create', { method: "post", body: [{
       "title": newTask.value,
       "completed": false
     }],
     async onResponse() {
       await refreshNuxtData("tasks")
+      newTask.value = ""
+      isAdding.value = false
     }})
   }
 
   async function completeTask(todo) {
+    isCompleting.value = true
+    isCompletingUuid.value = todo._uuid
     await useFetch('/api/task', { method: "put", body: [{
       "_uuid": todo._uuid,
       "completed": true
     }],
     async onResponse() {
+      isCompleting.value = false
+      isCompletingUuid.value = ""
       await refreshNuxtData("tasks")
     }})
   }
 
   async function deleteTask(uuid) {
+    isDeleting.value = true
+    isDeletingUuid.value = uuid
     await useFetch('/api/task', { method: "delete", body: { uuid },
     async onResponse() {
+      isDeleting.value = false
+      isDeletingUuid.value = ""
       await refreshNuxtData("tasks")
     }})
   }
@@ -39,24 +55,24 @@
 <template>
   <div class="tasks-app-container">
     <h1>Tasks App</h1>
-    <form>
+    <form @submit.prevent="addTask()">
       <label>New task </label>
       <input v-model="newTask" name="newTask" autocomplete="off" />
-      <button @click="addTask()" :disabled="newTask.length <= 0" >Add</button>
+      <button type="submit" :disabled="newTask.length <= 0" >{{ isAdding ? "Adding task..." : "Add" }}</button>
     </form>
     
     <h2>Task List</h2>
     <ul>
-      <li v-for="(task, index) in tasks" :key="index">
+      <li v-for="(task, index) in tasks || []" :key="index" v-bind:key="task._uuid">
         <span :class="task.completed ? 'completed-task': '' ">{{ task.title }}</span>
         <div class="action-buttons">
-          <button class="complete" @click="completeTask(task)" v-if="!task.completed">Complete</button>
-          <button class="delete" @click="deleteTask(task._uuid)">Delete</button>
+          <button class="complete" @click="completeTask(task)" v-if="!task.completed">{{ isCompleting && isCompletingUuid === task._uuid ? "Completing..." : "Complete" }}</button>
+          <button class="delete" @click="deleteTask(task._uuid)">{{ isDeleting && isDeletingUuid === task._uuid ? "Deleting..." : "Delete" }}</button>
         </div>
       </li>
     </ul>
 
-    <h4 v-if="Object.keys(tasks).length === 0">No tasks found</h4>
+    <h4 v-if="Object.keys(tasks || {}).length === 0">No tasks found</h4>
   </div>
 </template>
 
